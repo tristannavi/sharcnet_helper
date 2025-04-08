b7e538ae51495fe75adacf4a78b44f43801ba929
Tristan <17625517+tristannavi@users.noreply.github.com>
2025-04-08 11:58:23 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sharcnet_helper',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='GPL',
+    author='Tristan',
+    author_email='',
+    description=''
+)
---
+++ src/sbatch_utils.py
@@ -0,0 +1,105 @@
+import re
+import textwrap
+from pathlib import Path
+from typing import List, Any
+
+
+def make_job_name(sep: str, *args: Any) -> str:
+    """
+    Create a job name from the arguments.
+    :param sep: the separator to use
+    :type sep: str, required
+    :param args: the arguments to use
+    :type args: Any, required
+    :rtype: str
+    """
+    return sep.join(str(arg) for arg in args)
+
+
+class Directives:
+    def __init__(self, mem: str, hours: int, modules: List[str], working_dir: Path = Path(__file__).parent,
+                 minutes: int = 0, job_name: [str | None] = None, array_job: [int | List[int] | None] = None,
+                 mail_type: [str | List[str] | None] = "FAIL"):
+        """
+        :param mem: the memory to allocate for the job
+        :type mem: str, required
+        :param hours: the number of hours to allocate for the job
+        :type hours: int, required
+        :param minutes: the number of minutes to allocate for the job
+        :type minutes: int, optional
+        :param working_dir: the working directory for the job, defaults to the current directory
+        :type working_dir: Path, optional
+        :param modules: the modules to load for the job
+        :type modules: List[str], required
+        :param job_name: the name of the job
+        :type job_name: str | None, optional
+        :param array_job: if this is an array job, the number of runs in the array
+        :type array_job: int | List[int] | None, optional
+        :param mail_type: the type of email to send. ALL, BEGIN, END, FAIL, REQUEUE, TIME_LIMIT, TIME_LIMIT_90, defaults to FAIL
+        :type mail_type: str | List[str] | None, optional
+        :rtype: None
+        """
+
+        self.mem = mem
+        self.hours = hours
+        self.minutes = minutes
+        self.working_dir = working_dir
+        self.modules = modules
+        self.job_name = job_name
+        self.array_job = array_job
+        self.mail_type = mail_type
+
+    def make_directives(self) -> str:
+        def array_job_fn():
+            if self.array_job is None:
+                return ""
+            elif type(self.array_job) == list:
+                return "#SBATCH --array=" + ",".join(str(i) for i in self.array_job)
+            else:
+                return f"#SBATCH --array=1-{str(self.array_job)}"
+
+        directives = textwrap.dedent(f'''\
+                    #!/bin/bash
+                    #SBATCH --time={str(self.hours)}:{str(self.minutes) if self.minutes > 0 else "00"}:00
+                    #SBATCH --account=def-houghten
+                    #SBATCH --mem={self.mem}
+                    {array_job_fn()}
+                    #SBATCH --mail-user=tn13bm@brocku.ca
+                    #SBATCH --mail-type={self.mail_type if type(self.mail_type) == str else ','.join(self.mail_type)}
+                    #SBATCH --output={self.working_dir.absolute()}/output/slurm_{self.job_name}-{"%A_%a" if self.array_job is not None else "%A"}.out
+                    #SBATCH --job-name={self.job_name}-{"%A_%a" if self.array_job is not None else "%A"}.out
+                    
+                    mkdir -p {self.working_dir.absolute()}/output
+                    module load {' '.join(self.modules)}
+                ''')
+
+        return directives.replace("\n\n", "\n", 1)
+
+    def __str__(self) -> str:
+        """
+        Return the directives as a string.
+        :rtype: str
+        """
+        return self.make_directives()
+
+
+def make_batch_file(directives: Directives, commands: List[str], file_name: str = "sbatch.sh") -> None:
+    """
+    Create a batch file for the job.
+    :param directives: the directives to use
+    :type directives: Directives, required
+    :param commands: the commands to run
+    :type commands: str, required
+    :param file_name: the name of the file to create
+    :type file_name: str, optional
+    :rtype: None
+    """
+    with open(file_name, "w") as f:
+        f.write(str(directives) + "\n")
+        for command in commands:
+            variables = re.findall(r"\$[0-9]*", command)
+            print(variables)
+            for var in variables:
+                command = command.replace(var, f'"{var}"')
+                command = command.replace('""', '"')
+            f.write(command + "\n")
