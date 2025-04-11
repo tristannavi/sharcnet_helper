14d410ad1809eff1415b1e6fbf5678f693200275
Tristan <17625517+tristannavi@users.noreply.github.com>
2025-04-11 12:43:00 -0400

    Add sleep_and_write function and improve command handling
    Introduced the `sleep_and_write` function to monitor job queues, adding IO operations for stability. Enhanced command processing by modifying Python commands to flush output and properly escaping variables to avoid Bash misinterpretation.

--- src/sharcnet_helper/sbatch_utils.py
+++ src/sharcnet_helper/sbatch_utils.py
@@ -1,6 +1,9 @@
 import re
+import shlex
+import subprocess
 import textwrap
 from pathlib import Path
+from time import sleep
 from typing import List, Any
 
 
@@ -98,8 +101,37 @@ def make_batch_file(directives: Directives, commands: List[str], file_name: str 
         f.write(str(directives) + "\n")
         for command in commands:
             variables = re.findall(r"\$[0-9]*", command)
-            print(variables)
-            for var in variables:
+            command = re.sub("^python", "python -u", command)  # Add -u to the python command to flush output
+            for var in variables:  # Replace $1 with "$1" to prevent bash from interpreting it
                 command = command.replace(var, f'"{var}"')
                 command = command.replace('""', '"')
             f.write(command + "\n")
+
+
+def sleep_and_write(user: str, hours: float = 0.5) -> int:
+    """
+    Checks if there are still matching jobs in the queue every 30 minutes.
+    Create and delete a temp folder to ensure IO operations are done.
+
+    :param user: The username to check the queue for
+    :param hours: Number of hours to sleep, default is 0.5 (30 minutes)
+    :type hours: float, optional
+    :return: 0 to reset the counter
+    :rtype: int
+    """
+    while True:
+        result = subprocess.run(
+            f"squeue -u {user} | grep {user}",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True  # get output as a string rather than bytes
+        )
+
+        if result.returncode != 0:
+            break
+
+        sleep(hours * 60 * 60)
+        subprocess.call(shlex.split(f"mkdir -p temp"))
+        subprocess.call(shlex.split(f"rm -rf temp"))
+    return 0
